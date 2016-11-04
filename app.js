030664fe7be5013c68a8f6f4f0b1e0495343466f
tripu <t@tripu.info>
2016-11-04 11:45:54 +0100

    Use octonode methods (fixes #4). Simplify and compact code. Handle errors when there are no commmits/contributors/PRs/etc. Fetch API rate limit and display, together with time until reset.

--- app.js
+++ app.js
@@ -8,9 +8,9 @@
 const http = require('http');
 
 // External packages:
-const express = require('express'),
-    octonode = require('octonode'),
-    CLIProgressBar = require('cli-progress-bar');
+const CLIProgressBar = require('cli-progress-bar'),
+    express = require('express'),
+    octonode = require('octonode');
 
 // Settings:
 const conf = require('./conf');
@@ -84,97 +84,62 @@ function fetchProjectData(projectName) {
         }
     }
 
-    // get and store
-    //   creation date
-    // + description
-    // + repository Url
-    // + website Url
-    client.get(`/repos/${conf.organisation}/${projectName}`, {}, function (err, status, body, headers) {
-        projectsData[projectName]['github_url'] = 'https://github.com/w3c/' + projectName;
-        if(err) {
-            console.log(err);
-        } else {
-            try {
-                projectsData[projectName]["created_on"] = body["created_at"];
-            } catch (e) {
-                console.log(e);
-            }
-            try {
-                projectsData[projectName]["description"] = body["description"];
-            } catch (e) {
-                console.log(e);
-            }
-            try {
-                projectsData[projectName]['website_url'] = body['homepage'];
-            } catch (e) {
-                console.log(e);
-            }
-        }
+    repo.info((err, data, headers) => {
+        projectsData[projectName]['github_url'] = `https://github.com/w3c/${projectName}/`;
+        if (err)
+            return console.log(err);
+        projectsData[projectName]['created_at'] = (data && data.length > 0) ? data[0]['created_at'] : '';
+        projectsData[projectName].description = (data && data.length > 0) ? data[0].description : '';
+        projectsData[projectName].homepage = (data && data.length > 0) ? data[0].homepage : '';
         updateProgress();
     });
 
-    // get last commit
     repo.commits((err, data, headers) => {
-        if(err)
-            return console.log(err);
+        if (err)
+            return projectsData[projectName]['last_commit_on'] = '';
         if (data && data.length > 0 && data[0].commit && data[0].commit.author && data[0].commit.author.date)
             projectsData[projectName]['last_commit_on'] = data[0].commit.author.date;
-        else
-            projectsData[projectName]['last_commit_on'] = '';
         updateProgress();
     });
 
-    // get number of issues
-    client.get(`/repos/${conf.organisation}/${projectName}` + '/issues', {}, function (err, status, body, headers) {
-        if(err) {
-            console.log(err);
-        } else {
-            try {
-                projectsData[projectName]["opened_issues"] = body.length;
-            } catch (e) {
-                console.log(e);
-            }
-        }
+    repo.issues((err, data, headers) => {
+        if (err)
+            return console.log(err);
+        projectsData[projectName]['opened_issues'] = data ? data.length : 0;
         updateProgress();
     });
 
-    // get number of contributors
-    client.get(`/repos/${conf.organisation}/${projectName}` + '/contributors', {}, function (err, status, body, headers) {
-        if(err) {
-            console.log(err);
-        } else {
-            try {
-                projectsData[projectName]["contributors"] = body.length;
-            } catch (e) {
-                console.log(e);
-            }
-        }
+    repo.contributors((err, data, headers) => {
+        if (err)
+            return projectsData[projectName].contributors = 0;
+        projectsData[projectName].contributors = data ? data.length : 0;
         updateProgress();
     });
 
-    // get number of pending pull requests
-    client.get(`/repos/${conf.organisation}/${projectName}` + '/pulls', {}, function (err, status, body, headers) {
-        if(err) {
-            console.log(err);
-        } else {
-            try {
-                projectsData[projectName]["pending_pull_requests"] = body.length;
-            } catch (e) {
-                console.log(e);
-            }
-        }
+    repo.prs((err, data, headers) => {
+        if (err)
+            return console.log(err);
+        projectsData[projectName]['pending_pull_requests'] = data ? data.length : 0;
         updateProgress();
     });
 
-    // get last release
     repo.releases((err, data, headers) => {
-        if(err)
+        if (err)
             return console.log(err);
         projectsData[projectName]['last_release'] = (data && data.length > 0) ? data[0]['tag_name'] : '';
         updateProgress();
     });
 
 }
+
+client.limit((err, left, max, reset) => {
+    if (err)
+        return console.log(err);
+    const countdown = Math.round((reset * 1000 - new Date().getTime()) / 1000),
+        min = Math.floor(countdown / 60),
+        sec = countdown % 60;
+    console.log(`GH API, token ${conf.token}: ${left}/${max} (${Math.round(100*left/max)}%) requests available; resets in ${min}′${sec}″`);
+});
 
 fetchListOfProjects(() => {
     projectsList.sort((a, b) => {
@@ -191,8 +156,8 @@ fetchListOfProjects(() => {
 });
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
 });
 
 app.get('/projects', function(req,res) {
@@ -200,7 +165,7 @@ app.get('/projects', function(req,res) {
 });
 
 app.get('/api/*', function(req, res) {
-    if(req.url) {
+    if (req.url) {
         console.log(req.url);
         client.get(req.url.split('api')[1], {}, function(err, status, body, headers) {
             res.json(body);
@@ -209,7 +174,7 @@ app.get('/api/*', function(req, res) {
 });
 
 server.listen(conf.port, function() {
-    console.log(`listening on port ${conf.port}`);
+    console.log(`Listening on port ${conf.port}; visit http://localhost:${conf.port}/projects`);
     setInterval(function() {
         projectsList.map(fetchProjectData);
     }, conf.refreshPeriod * 60 * 1000);
